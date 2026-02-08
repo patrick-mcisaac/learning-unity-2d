44869840b9326e1128d25e11f67967e20f2c73f1
Patrick McIsaac <patrick.mcisaac.dev@gmail.com>
2026-02-08 16:11:59 -0600

    added controls for rocket

--- Assets/Scripts/Lander/Lander.cs
+++ Assets/Scripts/Lander/Lander.cs
@@ -1,23 +1,33 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour
 {
+    private Rigidbody2D landerRigidbody2D;
 
-    // left off at physics control
-    private void Update()
+    [SerializeField] private float rocketSpeed = 700f;
+    [SerializeField] private float turnSpeed = 100f;
+
+    private void Awake()
+    {
+        landerRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
     {
         if (Keyboard.current.upArrowKey.isPressed)
         {
-            Debug.Log("Up");
+            landerRigidbody2D.AddForce(rocketSpeed * transform.up * Time.deltaTime);
         }
         if (Keyboard.current.leftArrowKey.isPressed)
         {
-            Debug.Log("Left");
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
         }
         if (Keyboard.current.rightArrowKey.isPressed)
         {
-            Debug.Log("Right");
+            landerRigidbody2D.AddTorque(-turnSpeed * Time.deltaTime);
         }
     }
+
 }
